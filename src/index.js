43966d7d3ed4607caf2313af226e269a95d959e9
nuga99 <nugamuhammad@gmail.com>
2019-10-09 22:13:29 +0700

    Add nextState method for play

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ const AI_POINT_INDEX = 15;
 let houses = [];
 let turn = PLAYER;
 
-let stateGrab = 0;
-
 const isPoint = index => index === PLAYER_POINT_INDEX || index === AI_POINT_INDEX;
 const isValidIndex = index => index >= 0 && index <= MAX_HOUSE;
+
+
+let boardState = init(true);
+let currentPos = 0; // position still hardcode for test
+let grabSeed = boardState[currentPos%16];
+boardState[currentPos%16] = 0;
 
 export const init = (firstTurn = true) => {
   houses = [];
@@ -24,46 +28,28 @@ export const init = (firstTurn = true) => {
   turn = firstTurn ? PLAYER : AI;
 };
 
+
+export const nextState = () => {
+  boardState[(currentPos+1)%16] += 1;
+  currentPos+=1;
+  grabSeed-=1;
+}
+
 export const play = index => {
   if (!isValidIndex(index)) {
     throw new Error('Invalid house');
   }
+
+  while(grabSeed!==0){
+    getNextState();
+    if (grabSeed===0 && boardState[currentPos%16]!==1){
+      grabSeed = boardState[currentPos%16];
+      boardState[currentPos%16] = 0;
+    }
+  }
+
 };
 
 export const getState = () => houses.slice();
 
 export const getTurn = () => turn;
-
-export const nextStep = () => {
-
-  houses[count+1]+=1;
-	stateGrab -= 1;
-  count +=1;
-  
-}
-
-export const test_loop = () => {
-
-  while (stateGrab !== 0){
-	
-    console.log(stateGrab)
-    
-    if (stateGrab === 1 && houses[(count+houses[count])%16] === 0){
-      
-      break;
-    }
-    
-    else if (stateGrab === 1 && houses[(count+houses[count])%16] !== 0){
-      
-      stateGrab+=houses[(count+houses[count])%16];
-      console.log(stateGrab)
-    }
-    
-    else{
-      
-      nextStep();
-      console.log(houses);
-    }
-  }
-
-}
