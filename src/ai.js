21ef35e5d17a9f16c90357877e79674d81fac531
Bunga Amalia <bungasokia@gmail.com>
2019-11-03 18:12:33 +0700

    Add opposite rule implementation

--- src/ai.js
+++ src/ai.js
@@ -1,13 +1,17 @@
 const MAX_HOUSE = 16;
 const PLAYER_POINT_INDEX = 7;
+const AI_POINT_INDEX = 15;
+const OPPOSITE = [-1, -1, -1, -1, -1, -1, -1, -1, 6, 5, 4, 3, 2, 1, 0, -1];
 
 export const aiMove = (state, grabHouse) => {
-  if (state[grabHouse] == 0) {
+  if (state[grabHouse] == 0 || grabHouse == PLAYER_POINT_INDEX || grabHouse == AI_POINT_INDEX) {
     return state;
   }
 
-  let grabSeed = state[grabHouse];
+  let oneRound = false;
   let currentPos = grabHouse + 1;
+  let grabSeed = state[grabHouse];
+  state[grabHouse] = 0;
 
   while (grabSeed > 0) {
     if (currentPos == PLAYER_POINT_INDEX) {
@@ -15,7 +19,7 @@ export const aiMove = (state, grabHouse) => {
       continue;
     }
 
-    if (grabSeed == 1 && state[currentPos] != 0) {
+    if (currentPos != AI_POINT_INDEX && grabSeed == 1 && state[currentPos] != 0) {
       grabSeed += state[currentPos];
       state[currentPos] = 0;
     } else {
@@ -23,9 +27,17 @@ export const aiMove = (state, grabHouse) => {
       grabSeed -= 1;
     }
     currentPos = (currentPos + 1) % MAX_HOUSE;
+    if (currentPos == grabHouse) {
+      oneRound = true;
+    }
+  }
+
+  if (oneRound && OPPOSITE[currentPos] != -1) {
+    if (state[OPPOSITE[currentPos]] != 0) {
+      state[AI_POINT_INDEX] += state[OPPOSITE[currentPos]];
+      state[OPPOSITE[currentPos]] = 0;
+    }
   }
 
   return state;
 };
-
-export const getMove = () => 11;
