fb4453f1d67e693eea9c585b7d4b109653dd8513
Bunga Amalia <bungasokia@gmail.com>
2019-11-03 14:05:55 +0700

    Add aiMove and its demo

--- src/ai.js
+++ src/ai.js
@@ -1,3 +1,31 @@
-const getMove = () => 11;
+const MAX_HOUSE = 16;
+const PLAYER_POINT_INDEX = 7;
 
-export default getMove;
+export const aiMove = (state, grabHouse) => {
+  if (state[grabHouse] == 0) {
+    return state;
+  }
+
+  let grabSeed = state[grabHouse];
+  let currentPos = grabHouse + 1;
+
+  while (grabSeed > 0) {
+    if (currentPos == PLAYER_POINT_INDEX) {
+      currentPos += 1;
+      continue;
+    }
+
+    if (grabSeed == 1 && state[currentPos] != 0) {
+      grabSeed += state[currentPos];
+      state[currentPos] = 0;
+    } else {
+      state[currentPos] += 1;
+      grabSeed -= 1;
+    }
+    currentPos = (currentPos + 1) % MAX_HOUSE;
+  }
+
+  return state;
+};
+
+export const getMove = () => 11;
---
+++ src/demo.js
@@ -0,0 +1,5 @@
+import aiMove from './ai';
+
+const initialState = [7, 7, 7, 7, 7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 7, 0];
+
+console.log(aiMove(initialState, 10));
